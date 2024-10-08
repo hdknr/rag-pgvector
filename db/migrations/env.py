0ae81edf8b7b50ddcf648deec3bbd65af6fbb87c
user.name <gmail@hdknr.com>
2024-10-08 15:52:28 +0900

    .env

--- db/migrations/env.py
+++ db/migrations/env.py
@@ -4,10 +4,14 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from dotenv import load_dotenv
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+# .env
+load_dotenv()
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -64,9 +68,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
